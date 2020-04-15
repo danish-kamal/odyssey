1f078becf30473fdfe3f5fb82272f981a2d6c3bd
Arnold Sandoval <arnold.sandoval@okta.com>
2020-04-14 17:23:36 -0700

    update config, switch from nav to sidebar

--- packages/docs/source/.vuepress/config.js
+++ packages/docs/source/.vuepress/config.js
@@ -1,40 +1,54 @@
 // .vuepress/config.js
 
 module.exports = {
+  title: 'Odyssey',
+  description: 'The Okta design system',
   themeConfig: {
-    nav: [
+    sidebarDepth: 2,
+    lastUpdated: 'Last Updated',
+    repo: 'okta/odyssey',
+    repoLabel: 'okta/odyssey',
+    docsDir: 'packages/docs/source',
+    docsBranch: 'master',
+    editLinks: true,
+    editLinkText: 'Help us improve this page!',
+    sidebar: [
       {
-        text: 'Foundations',
-        items: [
-          { text: 'Color', link: '/foundations/color' },
-          { text: 'Elements', link: '/foundations/elements' },
-          { text: 'Spacing', link: '/foundations/spacing' },
-          { text: 'Type', link: '/foundations/type' },
+        title: 'Home',   // required
+        path: '/',      // optional, link of the title, which should be an absolute path and must exist
+      },
+      {
+        title: 'Foundations',
+        children: [
+          { title: 'Color', path: '/foundations/color' },
+          { title: 'Elements', path: '/foundations/elements' },
+          { title: 'Spacing', path: '/foundations/spacing' },
+          { title: 'Type', path: '/foundations/type' },
         ]
       },
       {
-        text: 'Components',
-        items: [
-          { text: 'Banner', link: '/components/banner' },
-          { text: 'Button', link: '/components/button' },
-          { text: 'Callout', link: '/components/callout' },
-          { text: 'Card', link: '/components/card' },
-          { text: 'Checkbox', link: '/components/checkbox' },
-          { text: 'Dropdown', link: '/components/dropdown' },
-          { text: 'Form', link: '/components/form' },
-          { text: 'Link', link: '/components/link' },
-          { text: 'Meter', link: '/components/meter' },
-          { text: 'Modal', link: '/components/modal' },
-          { text: 'Navigation', link: '/components/navigation' },
-          { text: 'Number Input', link: '/components/number-input' },
-          { text: 'Radio Button', link: '/components/radio-button' },
-          { text: 'Select', link: '/components/select' },
-          { text: 'Switch', link: '/components/switch' },
-          { text: 'Table', link: '/components/table' },
-          { text: 'Text Input', link: '/components/text-input' },
-          { text: 'Toast', link: '/components/toast' },
-          { text: 'Tooltip', link: '/components/tooltip' },
-          { text: 'Top Bar', link: '/components/top-bar' },
+        title: 'Components',
+        children: [
+          { title: 'Banner', path: '/components/banner' },
+          { title: 'Button', path: '/components/button' },
+          { title: 'Callout', path: '/components/callout' },
+          { title: 'Card', path: '/components/card' },
+          { title: 'Checkbox', path: '/components/checkbox' },
+          { title: 'Dropdown', path: '/components/dropdown' },
+          { title: 'Form', path: '/components/form' },
+          { title: 'Link', path: '/components/link' },
+          { title: 'Meter', path: '/components/meter' },
+          { title: 'Modal', path: '/components/modal' },
+          { title: 'Navigation', path: '/components/navigation' },
+          { title: 'Number Input', path: '/components/number-input' },
+          { title: 'Radio Button', path: '/components/radio-button' },
+          { title: 'Select', path: '/components/select' },
+          { title: 'Switch', path: '/components/switch' },
+          { title: 'Table', path: '/components/table' },
+          { title: 'Text Input', path: '/components/text-input' },
+          { title: 'Toast', path: '/components/toast' },
+          { title: 'Tooltip', path: '/components/tooltip' },
+          { title: 'Top Bar', path: '/components/top-bar' },
         ]
       }
     ]
